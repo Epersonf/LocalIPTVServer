c6d61dadb9f34579219e0d36440f321752f89133
Eperson Mayrink <epersoncmxf@gmail.com>
2026-04-25 00:24:50 -0300

    config fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
+// src/utils/config.js
 const path = require('path');
 
 const PORT = 8080;
 let HOST_BIND = '0.0.0.0';
 let SERVER_IP = '127.0.0.1';
 
-// Processa argumentos (--host=... --server-ip=...)
 const args = process.argv.slice(2);
 args.forEach(arg => {
   if (arg.startsWith('--host=')) {
@@ -14,6 +14,7 @@ args.forEach(arg => {
   }
 });
 
-const mediaPath = path.join(__dirname, '..', 'public');
+// CORREÇÃO AQUI: Subindo dois níveis (de src/utils para src, e de src para a raiz)
+const mediaPath = path.join(__dirname, '..', '..', 'public');
 
 module.exports = { PORT, HOST_BIND, SERVER_IP, mediaPath };
